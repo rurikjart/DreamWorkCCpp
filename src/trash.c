63d8ab997490da0acf5e3997f878266b148aa18c
Y A <rurik.j.art@gmail.com>
2022-07-11 09:02:42 +0000

    push

--- src/trash.c
+++ src/trash.c
@@ -8,3 +8,21 @@ void *pop (Element *stack);
 
 void push (Element **stack, void *data);
 void *pop (Element **stack);
+
+bool createStack(Element ** stack){
+	*stack = NULL;
+	return true;
+}
+
+bool push(Element **stack, void *data) {
+	Element *elem = malloc(sizeof(Element));
+	if (!elem) return false;
+	
+	
+	elem->data = data;
+	elem->next = *stack;
+	*stack = elem;
+	
+	return true;
+}
+
